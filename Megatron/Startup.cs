9e0da1d3f1395de791a776da1dfd31235bc0cdea
SuperMido <supermido1996@gmail.com>
2021-01-27 00:59:46 +0700

    MEGATRON-44 #Update ConnectionString for Sql Server

--- Megatron/Startup.cs
+++ Megatron/Startup.cs
@@ -29,7 +29,7 @@ namespace Megatron
         {
             services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseSqlServer(
-                    Configuration.GetConnectionString("DefaultConnection")));
+                    Configuration.GetConnectionString("SqlServerDocker")));
             services.AddDatabaseDeveloperPageExceptionFilter();
 
             services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
