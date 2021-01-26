a65d2d1b475e167a8d7b2e9e5cfc1253c250db51
SuperMido <supermido1996@gmail.com>
2021-01-27 01:15:55 +0700

    MEGATRON-46 #Set up Role and Initializer

---
+++ Megatron/Utility/SystemRoles.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Megatron.Utility
+{
+    public class SystemRoles
+    {
+        public const string Administrator = "Administrator";
+        public const string MarketingManager = "Marketing Manager";
+        public const string MarketingCoordinator = "Marketing Coordinator";
+        public const string Student = "Student";
+        public const string Guest = "Guest";
+    }
+}
