47273216b12820387d72aa373ad70deebd93c7ab
Huy Tran <huy.quang.tran@mgm-tp.com>
2021-02-03 15:00:25 +0700

    MEGATRON-6 Admin Create Account for Student, MC, MM, Guest

--- Megatron/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ Megatron/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -5,6 +5,8 @@ using System.Linq;
 using System.Text;
 using System.Text.Encodings.Web;
 using System.Threading.Tasks;
+using Megatron.Models;
+using Megatron.Utility;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
@@ -24,24 +26,20 @@ namespace Megatron.Areas.Identity.Pages.Account
         private readonly ILogger<RegisterModel> _logger;
         private readonly IEmailSender _emailSender;
 
-        public RegisterModel(
-            UserManager<IdentityUser> userManager,
-            SignInManager<IdentityUser> signInManager,
-            ILogger<RegisterModel> logger,
-            IEmailSender emailSender)
+        public RegisterModel(SignInManager<IdentityUser> signInManager, UserManager<IdentityUser> userManager, ILogger<RegisterModel> logger, IEmailSender emailSender)
         {
-            _userManager = userManager;
             _signInManager = signInManager;
+            _userManager = userManager;
             _logger = logger;
             _emailSender = emailSender;
         }
 
-        [BindProperty]
+        [BindProperty] 
         public InputModel Input { get; set; }
 
         public string ReturnUrl { get; set; }
 
-        public IList<AuthenticationScheme> ExternalLogins { get; set; }
+        private IList<AuthenticationScheme> ExternalLogins { get; set; }
 
         public class InputModel
         {
@@ -49,6 +47,10 @@ namespace Megatron.Areas.Identity.Pages.Account
             [EmailAddress]
             [Display(Name = "Email")]
             public string Email { get; set; }
+            
+            [Display(Name = "Confirm Email")]
+            [Compare("Email", ErrorMessage = "The Email and confirmation Email do not match.")]
+            public string ConfirmEmail { get; set; }
 
             [Required]
             [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
@@ -60,54 +62,58 @@ namespace Megatron.Areas.Identity.Pages.Account
             [Display(Name = "Confirm password")]
             [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
             public string ConfirmPassword { get; set; }
-        }
 
-        public async Task OnGetAsync(string returnUrl = null)
-        {
-            ReturnUrl = returnUrl;
-            ExternalLogins = (await _signInManager.GetExternalAuthenticationSchemesAsync()).ToList();
+            public string FullName { get; set; }
+
+            [Display(Name = "Phone Number")]
+            public string PhoneNumber { get; set; }
         }
 
         public async Task<IActionResult> OnPostAsync(string returnUrl = null)
         {
+            string role = Request.Form["rdUserRole"].ToString();
             returnUrl ??= Url.Content("~/");
             ExternalLogins = (await _signInManager.GetExternalAuthenticationSchemesAsync()).ToList();
             if (ModelState.IsValid)
             {
-                var user = new IdentityUser { UserName = Input.Email, Email = Input.Email };
+                var user = new ApplicationUser
+                {
+                    UserName = Input.FullName,
+                    Email = Input.Email,
+                    PhoneNumber = Input.PhoneNumber
+                };
                 var result = await _userManager.CreateAsync(user, Input.Password);
                 if (result.Succeeded)
                 {
-                    _logger.LogInformation("User created a new account with password.");
-
-                    var code = await _userManager.GenerateEmailConfirmationTokenAsync(user);
-                    code = WebEncoders.Base64UrlEncode(Encoding.UTF8.GetBytes(code));
-                    var callbackUrl = Url.Page(
-                        "/Account/ConfirmEmail",
-                        pageHandler: null,
-                        values: new { area = "Identity", userId = user.Id, code = code, returnUrl = returnUrl },
-                        protocol: Request.Scheme);
-
-                    await _emailSender.SendEmailAsync(Input.Email, "Confirm your email",
-                        $"Please confirm your account by <a href='{HtmlEncoder.Default.Encode(callbackUrl)}'>clicking here</a>.");
-
-                    if (_userManager.Options.SignIn.RequireConfirmedAccount)
+                    switch (role)
                     {
-                        return RedirectToPage("RegisterConfirmation", new { email = Input.Email, returnUrl = returnUrl });
+                        case SystemRoles.MarketingManager when (User.IsInRole(SystemRoles.Administrator)):
+                            await _userManager.AddToRoleAsync(user, SystemRoles.MarketingManager);
+                            break;
+                        case SystemRoles.MarketingCoordinator when (User.IsInRole(SystemRoles.Administrator)):
+                            await _userManager.AddToRoleAsync(user, SystemRoles.MarketingCoordinator);
+                            break;
+                        case SystemRoles.Student when (User.IsInRole(SystemRoles.Administrator)):
+                            await _userManager.AddToRoleAsync(user, SystemRoles.Student);
+                            break;
+                        case SystemRoles.Guest when (User.IsInRole(SystemRoles.Administrator)):
+                            await _userManager.AddToRoleAsync(user, SystemRoles.Guest);
+                            break;
+                        default:
+                            await _userManager.DeleteAsync(user);
+                            break;
                     }
-                    else
+                    foreach (var error in result.Errors)
                     {
-                        await _signInManager.SignInAsync(user, isPersistent: false);
-                        return LocalRedirect(returnUrl);
+                        ModelState.AddModelError(string.Empty, error.Description);
                     }
+                    return RedirectToAction("Index", "Admin");
                 }
                 foreach (var error in result.Errors)
                 {
                     ModelState.AddModelError(string.Empty, error.Description);
                 }
             }
-
-            // If we got this far, something failed, redisplay form
             return Page();
         }
     }
