d29229948a2aa0158b617f1dff67635d78085676
thaihieuhuynh1752 <thaihieuhuynh1752@gmail.com>
2021-02-05 10:50:08 +0700

    Merge remote-tracking branch 'origin/HieuTest' into HieuTest

---
+++ SeleniumAutomationTests/UnitTest1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace SeleniumAutomationTests
+{
+    [TestClass]
+    public class TestLogin
+    {
+        [TestMethod]
+        public void TestLoginMethod()
+        {
+            string url = "https://localhost:5001";
+            try
+            {
+                IWebDriver driver = new ChromeDriver("C:/WebDriver/bin/");
+                driver.Url = url;
+                driver.Navigate().GoToUrl(url);
+                driver.Quit();
+            }
+            catch
+            {
+                IWebDriver driver = new ChromeDriver("/usr/bin/chromedriver");
+                driver.Url = url;
+                driver.Navigate().GoToUrl(url);
+                driver.Quit();
+            }
+            
+            
+        }
+    }
+
+    [TestClass]
+    public class TestSubmitArticle
+    {
+        [TestMethod]
+        public void TestSubmitArticleMethod()
+        {
+            // IWebDriver driver = new ChromeDriver("C:/WebDriver/bin/");
+            // driver.Navigate().GoToUrl("https://localhost:5001/student/submitarticle");
+            // SubmitArticlePage submitArticlePage = new SubmitArticlePage(driver);
+            // HomePage homePage = submitArticlePage.SubmitArticle("Title so 1", "<h1>Content ne</h1>", "1");
+            // Assert.Equals(homePage.GetMessageText(), ("Article submitted"));
+        }
+    }
+}
