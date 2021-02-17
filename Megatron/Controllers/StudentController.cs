677ad6e01faa527c7c597b5723f35a8216901861
SuperMido <supermido1996@gmail.com>
2021-02-18 00:12:43 +0700

    MEGATRON-28 Students upload pictures

--- Megatron/Controllers/StudentController.cs
+++ Megatron/Controllers/StudentController.cs
@@ -1,21 +1,22 @@
-﻿using Megatron.Models;
-using Megatron.Services;
+﻿using Megatron.Services;
 using Megatron.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using System.IO;
+using Megatron.Extensions;
+using Microsoft.AspNetCore.Hosting;
 
 namespace Megatron.Controllers
 {
     public class StudentController : Controller
     {
         private readonly IStudentRepository _studentRepository;
+        private readonly IWebHostEnvironment _webHostEnvironment;
 
-        public StudentController(IStudentRepository studentRepository)
+        public StudentController(IStudentRepository studentRepository, IWebHostEnvironment webHostEnvironment)
         {
             _studentRepository = studentRepository;
+            _webHostEnvironment = webHostEnvironment;
         }
 
         public IActionResult Index()
@@ -42,6 +43,50 @@ namespace Megatron.Controllers
 
             ViewData["Message"] = articleSubmit.StatusMessage;
             return View(articleSubmit);
+        }
+        
+        [HttpPost]
+        public ActionResult UploadImage()
+        {                  
+            var files = Request.Form.Files;
+            if (files.Count == 0 || !files[0].IsImage())
+            {
+                var rError = new
+                {
+                    uploaded = false,
+                    url = string.Empty
+                };
+                return Json(rError);
+            }
+            var formFile = files[0];
+            var upFileName = formFile.FileName;
+            var fileName = Guid.NewGuid() + Path.GetExtension(upFileName);
+            var saveDir = Path.Combine(_webHostEnvironment.WebRootPath, "images");
+            var savePath = Path.Combine(saveDir, fileName);
+            var previewPath = "/images/" + fileName;
+ 
+            var result = true;
+            try
+            {
+                if (!Directory.Exists(saveDir))
+                {
+                    Directory.CreateDirectory(saveDir);
+                }
+
+                using var fs = System.IO.File.Create(savePath);
+                formFile.CopyTo(fs);
+                fs.Flush();
+            }
+            catch (Exception ex)
+            {
+                result = false;
+            }
+            var rUpload = new
+            {
+                uploaded = result,
+                url = result ? previewPath : string.Empty
+            };
+            return Json(rUpload);
         }
     }
 }
---
+++ Megatron/wwwroot/lib/ckeditor5/src/ckeditor.js
@@ -0,0 +1,75 @@
+/**
+ * @license Copyright (c) 2014-2021, CKSource - Frederico Knabben. All rights reserved.
+ * For licensing, see LICENSE.md or https://ckeditor.com/legal/ckeditor-oss-license
+ */
+import ClassicEditor from '@ckeditor/ckeditor5-editor-classic/src/classiceditor.js';
+import Alignment from '@ckeditor/ckeditor5-alignment/src/alignment.js';
+import AutoImage from '@ckeditor/ckeditor5-image/src/autoimage.js';
+import Autoformat from '@ckeditor/ckeditor5-autoformat/src/autoformat.js';
+import BlockQuote from '@ckeditor/ckeditor5-block-quote/src/blockquote.js';
+import Bold from '@ckeditor/ckeditor5-basic-styles/src/bold.js';
+import CKFinder from '@ckeditor/ckeditor5-ckfinder/src/ckfinder.js';
+import CKFinderUploadAdapter from '@ckeditor/ckeditor5-adapter-ckfinder/src/uploadadapter.js';
+import Essentials from '@ckeditor/ckeditor5-essentials/src/essentials.js';
+import ExportToPDF from '@ckeditor/ckeditor5-export-pdf/src/exportpdf.js';
+import ExportToWord from '@ckeditor/ckeditor5-export-word/src/exportword.js';
+import FontColor from '@ckeditor/ckeditor5-font/src/fontcolor.js';
+import FontFamily from '@ckeditor/ckeditor5-font/src/fontfamily.js';
+import FontSize from '@ckeditor/ckeditor5-font/src/fontsize.js';
+import Heading from '@ckeditor/ckeditor5-heading/src/heading.js';
+import Image from '@ckeditor/ckeditor5-image/src/image.js';
+import ImageCaption from '@ckeditor/ckeditor5-image/src/imagecaption.js';
+import ImageResize from '@ckeditor/ckeditor5-image/src/imageresize.js';
+import ImageStyle from '@ckeditor/ckeditor5-image/src/imagestyle.js';
+import ImageToolbar from '@ckeditor/ckeditor5-image/src/imagetoolbar.js';
+import ImageUpload from '@ckeditor/ckeditor5-image/src/imageupload.js';
+import Indent from '@ckeditor/ckeditor5-indent/src/indent.js';
+import Italic from '@ckeditor/ckeditor5-basic-styles/src/italic.js';
+import Link from '@ckeditor/ckeditor5-link/src/link.js';
+import List from '@ckeditor/ckeditor5-list/src/list.js';
+import MediaEmbed from '@ckeditor/ckeditor5-media-embed/src/mediaembed.js';
+import Paragraph from '@ckeditor/ckeditor5-paragraph/src/paragraph.js';
+import PasteFromOffice from '@ckeditor/ckeditor5-paste-from-office/src/pastefromoffice';
+import Table from '@ckeditor/ckeditor5-table/src/table.js';
+import TableToolbar from '@ckeditor/ckeditor5-table/src/tabletoolbar.js';
+import TextTransformation from '@ckeditor/ckeditor5-typing/src/texttransformation.js';
+import WordCount from '@ckeditor/ckeditor5-word-count/src/wordcount.js';
+
+class Editor extends ClassicEditor {}
+
+// Plugins to include in the build.
+Editor.builtinPlugins = [
+	Alignment,
+	AutoImage,
+	Autoformat,
+	BlockQuote,
+	Bold,
+	CKFinder,
+	CKFinderUploadAdapter,
+	Essentials,
+	ExportToPDF,
+	ExportToWord,
+	FontColor,
+	FontFamily,
+	FontSize,
+	Heading,
+	Image,
+	ImageCaption,
+	ImageResize,
+	ImageStyle,
+	ImageToolbar,
+	ImageUpload,
+	Indent,
+	Italic,
+	Link,
+	List,
+	MediaEmbed,
+	Paragraph,
+	PasteFromOffice,
+	Table,
+	TableToolbar,
+	TextTransformation,
+	WordCount
+];
+
+export default Editor;
