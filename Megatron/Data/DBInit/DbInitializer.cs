718a22a296308744a317c0e265bdc8017ec30ddd
Huy Tran <huy.quang.tran@mgm-tp.com>
2021-02-19 14:23:28 +0700

    MEGATRON-66 Seed User Account data

--- Megatron/Data/DBInit/DbInitializer.cs
+++ Megatron/Data/DBInit/DbInitializer.cs
@@ -4,6 +4,7 @@ using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using Megatron.Models;
 
 namespace Megatron.Data.DBInit
 {
@@ -11,14 +12,16 @@ namespace Megatron.Data.DBInit
     {
         private readonly ApplicationDbContext _db;
         private readonly RoleManager<IdentityRole> _roleManager;
+        private readonly UserManager<IdentityUser> _userManager;
 
-        public DbInitializer(ApplicationDbContext db, RoleManager<IdentityRole> roleManager)
+        public DbInitializer(ApplicationDbContext db, RoleManager<IdentityRole> roleManager, UserManager<IdentityUser> userManager)
         {
             _db = db;
             _roleManager = roleManager;
+            _userManager = userManager;
         }
 
-        public void Initialize()
+        public async void Initialize()
         {
             try
             {
@@ -45,6 +48,107 @@ namespace Megatron.Data.DBInit
                 if (_db.Roles.Any(r => r.Name == role)) continue;
                 _roleManager.CreateAsync(new IdentityRole(role)).GetAwaiter().GetResult();
             }
+            
+            //Megatron Admin Account
+            if (_userManager.FindByEmailAsync("megatronadmin@gmail.com").Result == null)
+            {
+                var result = _userManager.CreateAsync(new ApplicationUser
+                {
+                    UserName = "megatronadmin@gmail.com",
+                    FullName = "Megatron Admin",
+                    Email = "megatronadmin@gmail.com",
+                    EmailConfirmed = true
+                }, "Password@123").GetAwaiter().GetResult();
+                if (result.Succeeded)
+                {
+                    var user = await _db.Users.FirstOrDefaultAsync(u => u.UserName == "megatronadmin@gmail.com");
+                    await _userManager.AddToRoleAsync(user, SystemRoles.Administrator);
+                }
+            }
+            //Megatron Marketing Manager Account
+            if (_userManager.FindByEmailAsync("megatronmm@gmail.com").Result == null)
+            {
+                var result = _userManager.CreateAsync(new ApplicationUser
+                {
+                    UserName = "megatronmm@gmail.com",
+                    FullName = "Megatron Marketing Manager",
+                    Email = "megatronmm@gmail.com",
+                    EmailConfirmed = true
+                }, "Password@123").GetAwaiter().GetResult();
+                if (result.Succeeded)
+                {
+                    var user = await _db.Users.FirstOrDefaultAsync(u => u.UserName == "megatronmm@gmail.com");
+                    await _userManager.AddToRoleAsync(user, SystemRoles.MarketingManager);
+                }
+            }
+            //Megatron Marketing Coordinator Account
+            if (_userManager.FindByEmailAsync("megatronmc@gmail.com").Result == null)
+            {
+                var result = _userManager.CreateAsync(new ApplicationUser
+                {
+                    UserName = "megatronmc@gmail.com",
+                    FullName = "Megatron Marketing Coordinator",
+                    Email = "megatronmc@gmail.com",
+                    EmailConfirmed = true
+                }, "Password@123").GetAwaiter().GetResult();
+                if (result.Succeeded)
+                {
+                    var user = await _db.Users.FirstOrDefaultAsync(u => u.UserName == "megatronmc@gmail.com");
+                    await _userManager.AddToRoleAsync(user, SystemRoles.MarketingCoordinator);
+                }
+            }
+            //Megatron student Account
+            if (_userManager.FindByEmailAsync("megatronstudent@gmail.com").Result == null)
+            {
+                var result = _userManager.CreateAsync(new ApplicationUser
+                {
+                    UserName = "megatronstudent@gmail.com",
+                    FullName = "Megatron Student",
+                    Email = "megatronstudent@gmail.com",
+                    EmailConfirmed = true
+                }, "Password@123").GetAwaiter().GetResult();
+                if (result.Succeeded)
+                {
+                    var user = await _db.Users.FirstOrDefaultAsync(u => u.UserName == "megatronstudent@gmail.com");
+                    await _userManager.AddToRoleAsync(user, SystemRoles.Student);
+                }
+            }
+            //Megatron Guest Account
+            if (_userManager.FindByEmailAsync("megatronguest@gmail.com").Result == null)
+            {
+                var result = _userManager.CreateAsync(new ApplicationUser
+                {
+                    UserName = "megatronguest@gmail.com",
+                    FullName = "Megatron Guest",
+                    Email = "megatronguest@gmail.com",
+                    EmailConfirmed = true
+                }, "Password@123").GetAwaiter().GetResult();
+                if (result.Succeeded)
+                {
+                    var user = await _db.Users.FirstOrDefaultAsync(u => u.UserName == "megatronguest@gmail.com");
+                    await _userManager.AddToRoleAsync(user, SystemRoles.Guest);
+                }
+            }
+            
+            // If you want to create an new User, Duplicate the code below and change the account data
+            // Data example from here (Copy from here)
+            // Tran Quang Huy Account
+            if (_userManager.FindByEmailAsync("hqtran@gmail.com").Result == null)
+            {
+                var result = _userManager.CreateAsync(new ApplicationUser
+                {
+                    UserName = "hqtran@gmail.com",
+                    FullName = "Quang Huy",
+                    Email = "hqtran@gmail.com",
+                    EmailConfirmed = true
+                }, "Password@123").GetAwaiter().GetResult();
+                if (result.Succeeded)
+                {
+                    var user = await _db.Users.FirstOrDefaultAsync(u => u.UserName == "hqtran@gmail.com");
+                    await _userManager.AddToRoleAsync(user, SystemRoles.Administrator);
+                }
+            }
+            //To here
         }
     }
 }
--- Megatron/Startup.cs
+++ Megatron/Startup.cs
@@ -2,19 +2,12 @@ using Megatron.Data;
 using Megatron.Data.DBInit;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Identity.UI;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Megatron.Services;
-using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace Megatron
 {
