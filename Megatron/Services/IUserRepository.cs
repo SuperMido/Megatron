65f089531403fd0c5c178e16a7c5c9a8db6040de
Huy Tran <huy.quang.tran@mgm-tp.com>
2021-02-21 23:54:39 +0700

    MEGATRON-10 Admin View account list

--- Megatron/Services/IUserRepository.cs
+++ Megatron/Services/IUserRepository.cs
@@ -1,9 +1,14 @@
-﻿using System.Threading.Tasks;
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Megatron.ViewModels;
 
 namespace Megatron.Services
 {
     public interface IUserRepository
     {
         public Task<string> GetUserFullName(string userId);
+
+        public IEnumerable<UserRoleViewModel> GetAllUsers();
+        
     }
 }
