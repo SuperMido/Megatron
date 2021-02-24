735a438505218b0180e287c1cc04b99460713aa6
SuperMido <supermido1996@gmail.com>
2021-02-24 14:56:05 +0700

    MEGATRON-67 Assign user into faculty

---
+++ Megatron/Data/Migrations/20210222170755_UpdateCreateAt.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Megatron.Data.Migrations
+{
+    public partial class UpdateCreateAt : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreateAt",
+                table: "UserFaculties",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreateAt",
+                table: "Semesters",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreateAt",
+                table: "SemesterArticles",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreateAt",
+                table: "Faculties",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CreateAt",
+                table: "UserFaculties");
+
+            migrationBuilder.DropColumn(
+                name: "CreateAt",
+                table: "Semesters");
+
+            migrationBuilder.DropColumn(
+                name: "CreateAt",
+                table: "SemesterArticles");
+
+            migrationBuilder.DropColumn(
+                name: "CreateAt",
+                table: "Faculties");
+        }
+    }
+}
