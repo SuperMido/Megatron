e8ad304e94a9407212d60712018e4c7d3091b5e3
Trần Quang Huy <50128638+SuperMido@users.noreply.github.com>
2021-03-07 00:19:11 +0700

    MEGATRON-108 Assign name of MC when approve or decline (#73)
    * MEGATRON-108 Assign name of MC when approve or decline
    
    * Fix missing using

--- Megatron/Controllers/ArticleController.cs
+++ Megatron/Controllers/ArticleController.cs
@@ -9,12 +9,12 @@ namespace Megatron.Controllers
 {
     public class ArticleController : Controller
     {
-
         private readonly IArticleRepository _articleRepository;
         private readonly ICommentRepository _commentRepository;
         private readonly IUserRepository _userRepository;
 
-        public ArticleController(IArticleRepository articleRepository, ICommentRepository commentRepository, IUserRepository userRepository)
+        public ArticleController(IArticleRepository articleRepository, ICommentRepository commentRepository,
+            IUserRepository userRepository)
         {
             _articleRepository = articleRepository;
             _commentRepository = commentRepository;
@@ -22,69 +22,65 @@ namespace Megatron.Controllers
         }
 
         // GET
-        [Authorize(Roles = (SystemRoles.Administrator + "," + SystemRoles.MarketingCoordinator + "," + SystemRoles.MarketingManager))]
+        [Authorize(Roles = SystemRoles.Administrator + "," + SystemRoles.MarketingCoordinator + "," +
+                           SystemRoles.MarketingManager)]
         public IActionResult Index()
-        {            
+        {
             if (User.IsInRole(SystemRoles.MarketingCoordinator))
             {
                 var currentUser = User.FindFirstValue(ClaimTypes.NameIdentifier);
                 var facultyOfMc = _articleRepository.GetFacultiesForMC(currentUser);
-                return View(facultyOfMc);                
+                return View(facultyOfMc);
             }
-            else
-                return View(_articleRepository.GetFaculties());
+
+            return View(_articleRepository.GetFaculties());
         }
-        [Authorize(Roles = (SystemRoles.Administrator + "," + SystemRoles.MarketingCoordinator + "," + SystemRoles.MarketingManager))]
+
+        [Authorize(Roles = SystemRoles.Administrator + "," + SystemRoles.MarketingCoordinator + "," +
+                           SystemRoles.MarketingManager)]
         public IActionResult ListArticles(int id)
         {
             var listArticles = _articleRepository.GetListArticlesByFaculty(id);
             return View(listArticles);
         }
-        
-        [Authorize(Roles = (SystemRoles.Administrator + "," + SystemRoles.MarketingCoordinator + "," + SystemRoles.MarketingManager+ "," + SystemRoles.Student))]
+
+        [Authorize(Roles = SystemRoles.Administrator + "," + SystemRoles.MarketingCoordinator + "," +
+                           SystemRoles.MarketingManager + "," + SystemRoles.Student)]
         public IActionResult Details(int id)
         {
             var article = _articleRepository.GetArticleDetail(id);
             return View(article);
         }
-        
+
         public IActionResult GetListArticles(int id)
         {
             var listArticles = _articleRepository.GetListArticlesByFaculty(id);
             return new JsonResult(listArticles);
         }
-        
+
         [HttpPost]
-        [Authorize(Roles = (SystemRoles.Administrator + "," + SystemRoles.MarketingCoordinator))]
+        [Authorize(Roles = SystemRoles.Administrator + "," + SystemRoles.MarketingCoordinator)]
         public IActionResult UpdateArticleStatus(int articleId, bool status, string statusMessage)
         {
-            if (statusMessage == null)
-            {
-                return Json(new {success = false, message = "Please add a message!"});
-            }
-            if (_articleRepository.UpdateArticleStatus(articleId, status, statusMessage))
-            {
+            var currentUser = User.FindFirstValue(ClaimTypes.NameIdentifier);
+            var userFullname = _userRepository.GetUserFullName(currentUser).Result;
+            if (statusMessage == null) return Json(new {success = false, message = "Please add a message!"});
+            if (_articleRepository.UpdateArticleStatus(articleId, status, statusMessage, userFullname))
                 return Json(new {success = true, message = "Article has been approved"});
-            }
 
             return Json(new {success = false, message = "There are some error when approve!"});
         }
-        
+
         [HttpPost]
-        [Authorize(Roles = (SystemRoles.Administrator + "," + SystemRoles.Student + "," + SystemRoles.MarketingCoordinator))]
+        [Authorize(Roles =
+            SystemRoles.Administrator + "," + SystemRoles.Student + "," + SystemRoles.MarketingCoordinator)]
         public async Task<IActionResult> SendMessage(int articleId, string message)
         {
-            if (message == null)
-            {
-                return Json(new { success = false, message = "Please comment again!" });
-            }
+            if (message == null) return Json(new {success = false, message = "Please comment again!"});
             var currentUser = User.FindFirstValue(ClaimTypes.NameIdentifier);
             var userFullname = await _userRepository.GetUserFullName(currentUser);
             var sendMessage = await _commentRepository.SaveComment(articleId, currentUser, message);
-            if (sendMessage)
-            {
-                return Json(new { success = true, userInput = userFullname, messageInput = message });
-            }
+            if (sendMessage) return Json(new {success = true, userInput = userFullname, messageInput = message});
 
             return RedirectToAction(nameof(Index));
         }
--- Megatron/Models/Article.cs
+++ Megatron/Models/Article.cs
@@ -1,45 +1,38 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace Megatron.Models
 {
     public class Article
     {
-        [Key]
-        public int Id { get; set; }
-
-        [Required]
-        public string Title { get; set; }
-        
-        [Required]
-        public string Author { get; set; }
-
-        public string Content { get; set; }
-        
-        public string Image { get; set; }
-        
-        [Display(Name = "Faculty")]
-        public int FacultyId { get; set; }
-        
-        [ForeignKey("FacultyId")]
-        public virtual Faculty Faculty { get; set; }
-        
-        public bool Status { get; set; }
-        
-        public string StatusMessage { get; set; }
-        
-        public DateTime CreateAt { get; set; }
-        
-        public DateTime? UpdateAt { get; set; }
-
         public Article()
         {
             CreateAt = DateTime.Now;
             UpdateAt = DateTime.Now;
         }
+
+        [Key] public int Id { get; set; }
+
+        [Required] public string Title { get; set; }
+
+        [Required] public string Author { get; set; }
+
+        public string Content { get; set; }
+
+        public string Image { get; set; }
+
+        [Display(Name = "Faculty")] public int FacultyId { get; set; }
+
+        [ForeignKey("FacultyId")] public virtual Faculty Faculty { get; set; }
+
+        public bool Status { get; set; }
+
+        public string StatusMessage { get; set; }
+        public string ChangeStatusBy { get; set; }
+
+        public DateTime CreateAt { get; set; }
+
+        public DateTime? UpdateAt { get; set; }
     }
 }
