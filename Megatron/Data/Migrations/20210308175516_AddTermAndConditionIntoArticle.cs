5952b0d090f3bc88b89d610c501376ae2cb997bd
Ngoc Nguyen <ngocnqgcd18410@fpt.edu.vn>
2021-03-09 00:58:50 +0700

    MEGATRON-90 Student agree to Term and Conditions before they can submit

---
+++ Megatron/Data/Migrations/20210308175516_AddTermAndConditionIntoArticle.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Megatron.Data.Migrations
+{
+    public partial class AddTermAndConditionIntoArticle : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "TermCondition",
+                table: "Articles",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "TermCondition",
+                table: "Articles");
+        }
+    }
+}
