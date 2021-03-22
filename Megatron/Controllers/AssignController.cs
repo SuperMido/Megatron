baa4e9d7ecac09564abe6767f587cd4c7f3903e8
SuperMido <supermido1996@gmail.com>
2021-03-22 19:53:00 +0700

    MEGATRON-119 Update log data

--- Megatron/Controllers/AssignController.cs
+++ Megatron/Controllers/AssignController.cs
@@ -4,6 +4,7 @@ using Megatron.ViewModels;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System;
+using Microsoft.Extensions.Logging;
 
 namespace Megatron.Controllers
 {
@@ -11,16 +12,19 @@ namespace Megatron.Controllers
     public class AssignController : Controller
     {
         private readonly IUserRepository _userRepository;
+        private readonly ILogger<AssignController> _logger;
 
-        public AssignController(IUserRepository userRepository)
+        public AssignController(IUserRepository userRepository, ILogger<AssignController> logger)
         {
             _userRepository = userRepository;
+            _logger = logger;
         }
 
         // GET
         public IActionResult Index()
         {
             var usersInFaculty = _userRepository.GetUserInFaculty();
+            _logger.LogInformation("Display list user in each faculty!");
             return View(usersInFaculty);
         }
 
@@ -29,31 +33,37 @@ namespace Megatron.Controllers
             var mcInFaculty = _userRepository.GetMcInFaculty();
             return View(mcInFaculty);
         }
-        
+
         [HttpPost]
         public IActionResult AssignMc(UserFacultyViewModel model)
         {
             if (_userRepository.AssignMcToFaculty(model))
             {
+                _logger.LogInformation("User has been assign into faculty!");
                 return RedirectToAction(nameof(Index));
             }
+
+            _logger.LogWarning("Error when assign user into faculty");
             ViewData["Message"] = "Error: Marketing Manager already exists in Faculty";
             return View(_userRepository.GetMcInFaculty());
         }
-        
+
         public IActionResult AssignGuest()
         {
             var guestInFaculty = _userRepository.GetGuestInFaculty();
             return View(guestInFaculty);
         }
-        
+
         [HttpPost]
         public IActionResult AssignGuest(UserFacultyViewModel model)
         {
             if (_userRepository.AssignGuestToFaculty(model))
             {
+                _logger.LogInformation("User has been assign into faculty!");
                 return RedirectToAction(nameof(Index));
             }
+
+            _logger.LogWarning("Error when assign user into faculty");
             ViewData["Message"] = "Error: Guest already exists in Faculty";
             return View(_userRepository.GetGuestInFaculty());
         }
@@ -63,13 +73,16 @@ namespace Megatron.Controllers
             var usersInFaculty = _userRepository.GetUserInFaculty();
             return new JsonResult(usersInFaculty);
         }
-        
+
         public ActionResult DeleteUserInFaculty(int id)
         {
             if (!_userRepository.DeleteUserInFaculty(id))
             {
+                _logger.LogWarning("Error when assign delete user in faculty");
                 throw new ArgumentException("Error");
             }
+
+            _logger.LogInformation("User has been removed from faculty!");
             return RedirectToAction("Index");
         }
     }
